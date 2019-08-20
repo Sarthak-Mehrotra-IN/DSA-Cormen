9c74c54ccd3df6638c246355ff62572594463cb9
Sarthak-Mehrotra-IN <sarthak.mehrotra@outlook.com>
2019-08-20 06:17:12 +0530

    Merge Sort

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -33,7 +33,44 @@ public class Main {
         }
         return A;
     }
+    public static int [] Merge(int [] A,int p,int q,int r){
+        //1,3,4,2,5
+        //p=0,q=2,r=4
+        int n1=q-p+1;
+        int n2=r-q;
+        int [] L=new int [n1+1];
+        int [] R=new int[n2+1];
+        L[n1]=1000000000;
+        R[n2]=1000000000;
+        for(int i=0;i<n1;i++)
+            L[i]=A[p+i];
+        for(int j=0;j<n2;j++)
+            R[j]=A[q+j+1];
+        int i=0;
+        int j=0;
+        for(int k=p;k<=r;k++){
+            if(L[i]<=R[j]){
+                A[k]=L[i];
+                i++;
+            }
+            else{
+                A[k]=R[j];
+                j++;
+            }
+        }
+        return A;
+    }
+    public static int [] MergeSort(int [] A,int p,int r){
+        if(p<r){
+            int q=(p+r)/2;
+            MergeSort(A,p,q);
+            MergeSort(A,q+1,r);
+            Merge(A,p,q,r);
+        }
+        return A;
+    }
     public static void main(String[] args){
-        System.out.println(Arrays.toString(InsertionSort(ReadArray())));
+        //System.out.println(Arrays.toString(InsertionSort(ReadArray())));
+        //System.out.println(Arrays.toString(MergeSort(ReadArray(),0,4)));
     }
 }
